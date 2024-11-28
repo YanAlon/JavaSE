585c7cab27d3ff5e718cb6b11ca64bd5a01697ef
YanAlon <1366480684@qq.com>
2024-11-28 08:25:57 +0800

    20241128The first on the morning

---
+++ src/com/itheima/day06API/JFrame03Guess.java
@@ -0,0 +1,29 @@
+package com.itheima.day06API;
+
+import javax.swing.*;
+
+public class JFrame03Guess {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setTitle("Guess Number");
+        jf.setDefaultCloseOperation(3);
+        jf.setLocationRelativeTo(null);
+        jf.setSize(400, 300);
+        jf.setAlwaysOnTop(true);
+        jf.setLayout(null);
+        // 文本内容
+        JLabel textJL = new JLabel("Please guess a number(0 < number < 100).");
+        textJL.setBounds(70, 50, 350, 20);
+        jf.add(textJL);
+        // 输入框
+        JTextField inputJTF = new JTextField();
+        inputJTF.setBounds(120, 100, 150, 20);
+        jf.add(inputJTF);
+        // 按钮
+        JButton guessJB = new JButton("Guess");
+        guessJB.setBounds(150, 150, 100, 20);
+        jf.add(guessJB);
+
+        jf.setVisible(true);
+    }
+}
---
+++ src/com/itheima/day06API/JFrame04DateAndTime.java
@@ -0,0 +1,30 @@
+package com.itheima.day06API;
+
+import javax.swing.*;
+
+public class JFrame04DateAndTime {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setSize(400, 300);
+        jf.setTitle("Date And Time");
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(3);
+        jf.setAlwaysOnTop(true);
+        jf.setLayout(null);
+        // 内容显示
+        JLabel dateJL = new JLabel("Date");
+        dateJL.setBounds(50, 50, 100, 20);
+        jf.add(dateJL);
+        JLabel dateMassageJL = new JLabel("YYYY年MM月DD日");
+        dateMassageJL.setBounds(50, 80, 200, 20);
+        jf.add(dateMassageJL);
+        JLabel timeJL = new JLabel("Time");
+        timeJL.setBounds(50, 150, 100, 20);
+        jf.add(timeJL);
+        JLabel timeMassageJL = new JLabel("HH : MM : SS");
+        timeMassageJL.setBounds(50, 180, 200, 20);
+        jf.add(timeMassageJL);
+
+        jf.setVisible(true);
+    }
+}
